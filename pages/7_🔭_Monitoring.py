e6bcb8b37d9a0bb6ab5aeb0783746e29ce175771
Kurnia Ramadhan <kramadhan@gmail.com>
2023-02-22 12:02:01 +0700

    update index

---
+++ pages/7_🔭_Monitoring.py
@@ -0,0 +1,49 @@
+#####################################################################################
+# Source code: Dashboard Bukan Amel                                                 #
+#-----------------------------------------------------------------------------------#
+# Dashboard ini dibuat oleh:                                                        #
+# Nama          : Kurnia Ramadhan, ST.,M.Eng                                        #
+# Jabatan       : Sub Koordinator Pengelolaan Informasi LPSE                        #
+# Instansi      : Biro Pengadaan Barang dan Jasa Setda Prov. Kalbar                 #
+# Email         : kramadhan@gmail.com                                               #
+# URL Web       : https://github.com/blogramadhan                                   #
+#-----------------------------------------------------------------------------------#
+# Hak cipta milik Allah SWT, source code ini silahkan dicopy, di download atau      #
+# di distribusikan ke siapa saja untuk bahan belajar, atau untuk dikembangkan lagi  #
+# lebih lanjut, btw tidak untuk dijual ya.                                          #
+#                                                                                   #
+# Jika teman-teman mengembangkan lebih lanjut source code ini, agar berkenan untuk  #
+# men-share code yang teman-teman kembangkan lebih lanjut sebagai bahan belajar     #
+# untuk kita semua.                                                                 #
+#-----------------------------------------------------------------------------------#
+# @ Pontianak, 2023                                                                 #
+#####################################################################################
+
+# Import Library
+import duckdb
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+# Import library currency
+from babel.numbers import format_currency
+# Import library Aggrid
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+# Import library Google Cloud Storage
+from google.oauth2 import service_account
+from google.cloud import storage
+# Import fungsi pribadi
+#from fungsi import *
+
+# Konfigurasi variabel lokasi UKPBJ
+daerah =    ["PROV. KALBAR", "KOTA PONTIANAK", "KAB. KUBU RAYA", "KAB. MEMPAWAH", "KOTA SINGKAWANG", "KAB. SAMBAS", 
+            "KAB. BENGKAYANG", "KAB. LANDAK", "KAB. SANGGAU", "KAB. SEKADAU", "KAB. SINTANG", "KAB. MELAWI", "KAB. KAPUAS HULU", 
+            "KAB. KAYONG UTARA", "KAB. KETAPANG"]
+
+tahuns = [2023, 2022]
+
+pilih = st.sidebar.selectbox("Pilih UKPBJ yang diinginkan :", daerah)
+tahun = st.sidebar.selectbox("Pilih Tahun :", tahuns)
+
+st.title("Halaman Monitoring")
+
