82b21f064888a9a65ce9a654b9afb154d8265dbe
Kurnia Ramadhan <kramadhan@gmail.com>
2023-09-21 14:09:38 +0700

    modifikasi data

--- pages/3_📋_Perencanaan.py
+++ pages/3_📋_Perencanaan.py
@@ -162,3 +162,5 @@ with menurup2:
     col3.metric(label="Belanja Modal", value=3000000)
     col4.metric(label="Belanja Total", value=6000000)  
     style_metric_cards()  
+    
+    st.markdown("#### **POSISI INPUT RUP**")
