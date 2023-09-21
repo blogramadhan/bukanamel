ac59b5773938c57f9ed69db24b27bdc612bf144b
Kurnia Ramadhan <kramadhan@gmail.com>
2023-09-21 13:33:07 +0700

    modifikasi code

--- fungsi.py
+++ fungsi.py
@@ -4,4 +4,4 @@ import pandas as pd
 # Fungsi-fungsi yang bisa digunakan
 ## Fungsi Download Dataframe ke CSV
 def unduh_data(unduhdata):
-    return unduhdata.to_excel(index=False).encode('utf')
+    return unduhdata.to_excel(index=False)
