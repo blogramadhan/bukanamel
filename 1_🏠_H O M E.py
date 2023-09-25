1d23fe26577813c6256d702c08e8f1ecc1930883
Kurnia Ramadhan <kramadhan@gmail.com>
2023-09-25 08:42:01 +0700

    update code

--- 1_🏠_H O M E.py
+++ 1_🏠_H O M E.py
@@ -32,26 +32,17 @@ st.title("Dashboard Bukan Amel")
 st.markdown("""
 *Dashboard* ini dibuat sebagai alat bantu untuk mempermudah para pelaku pengadaan di seluruh wilayah Provinsi Kalimantan Barat. Data yang disajikan, antara lain:
 * **Perencanaan**
-  * Rekap Perencanaan
-  * Detail Paket
-  * Rekap Satker
-* **Persiapan**
-  * Rekap Persiapan
-  * Detail Paket
-* **Pemilihan**
-  * Rekap Pemilihan
-  * Detail Paket
-* **Kontrak**
-  * Rekap Kontrak
-  * Detail Paket
-* **Serah Terima**
-  * Rekap Serah Terima
-  * Detail Paket
-* **Monitoring**
-  * Monitoring Paket
   * Struktur Anggaran
-  * Toko Daring
-  * Warning RUP
+  * Profil RUP Daerah
+  * Profil RUP Perangkat Daerah
+  * % Input RUP
+  * RUP Paket Penyedia Perangkat Daerah
+  * RUP Paket Swakelola Perangkat Daerah
+* **Persiapan**
+* **Pemilihan**
+* **Kontrak**
+* **Serah Terima**
+* **Monitoring**
 
 *Made with love* dengan menggunakan bahasa programming [Python](https://www.python.org/) dengan beberapa *library* utama seperti:
 * [Pandas](https://pandas.pydata.org/)
@@ -60,5 +51,5 @@ st.markdown("""
 
 Sumber data dari *Dashboard* ini berasal dari **API JSON** yang ditarik harian dari [ISB LKPP](https://lkpp.go.id). Data tersebut kemudian disimpan di [Google Cloud Storage](https://google.com) untuk kemudian diolah lebih lanjut dengan [Python](https://python.org).
 
-@2022 - **UlarKadut** 
+@2023 - **UlarKadut** 
 """)
