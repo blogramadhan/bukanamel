bb74e7430025647244c426f46bed97130358ac58
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2023-10-19 10:38:02 +0700

    update code SPSE

--- pages/3_🔭_SPSE.py
+++ pages/3_🔭_SPSE.py
@@ -35,11 +35,127 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 from fungsi import *
 
 # Konfigurasi variabel lokasi UKPBJ
-daerah = ["PROV. KALBAR"]
+daerah = ["PROV. KALBAR", "KAB. BENGKAYANG", "KAB. MELAWI", "KOTA PONTIANAK", "KAB. SANGGAU", "KAB. SEKADAU", "KAB. KAPUAS HULU", "KAB. KUBU RAYA"]
 
-tahuns = ["2023"]
+tahuns = ["2023", "2022"]
 
-#pilih = st.sidebar.selectbox("Pilih UKPBJ yang diinginkan :", daerah)
-#tahun = st.sidebar.selectbox("Pilih Tahun :", tahuns)
+pilih = st.sidebar.selectbox("Pilih UKPBJ yang diinginkan :", daerah)
+tahun = st.sidebar.selectbox("Pilih Tahun :", tahuns)
 
-st.title("Halaman SPSE")
+if pilih == "PROV. KALBAR":
+    kodeFolder = "prov"
+if pilih == "KAB. BENGKAYANG":
+    kodeFolder = "bky"
+if pilih == "KAB. MELAWI":
+    kodeFolder = "mlw"
+if pilih == "KOTA PONTIANAK":
+    kodeFolder = "ptk"
+if pilih == "KAB. SANGGAU":
+    kodeFolder = "sgu"
+if pilih == "KAB. SEKADAU":
+    kodeFolder = "skd"
+if pilih == "KAB. KAPUAS HULU":
+    kodeFolder = "kph"
+if pilih == "KAB. KUBU RAYA":
+    kodeFolder = "kkr"
+
+# Persiapan Dataset
+con = duckdb.connect(database=':memory:')
+
+## Akses file dataset format parquet dari Google Cloud Storage via URL Public
+
+### Dataset SPSE Tender
+DatasetSPSETenderPengumuman = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSETenderPengumuman{tahun}.parquet"
+DatasetSPSETenderSelesai = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSETenderSelesai{tahun}.parquet"
+DatasetSPSETenderSelesaiNilai = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSETenderSelesaiNilai{tahun}.parquet"
+DatasetSPSETenderSPPBJ = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSETenderEkontrakSPPBJ{tahun}.parquet"
+DatasetSPSETenderKontrak = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSETenderEkontrakKontrak{tahun}.parquet"
+DatasetSPSETenderSPMK = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSETenderEkontrakSPMKSPP{tahun}.parquet"
+DatasetSPSETenderBAST = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSETenderEkontrakBAPBAST{tahun}.parquet"
+
+### Dataset SPSE Non Tender
+DatasetSPSENonTenderPengumuman = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSENonTenderPengumuman{tahun}.parquet"
+DatasetSPSENonTenderSelesai = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSENonTenderSelesai{tahun}.parquet"
+DatasetSPSENonTenderSPPBJ = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSENonTenderEkontrakSPPBJ{tahun}.parquet"
+DatasetSPSENonTenderKontrak = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSENonTenderEkontrakKontrak{tahun}.parquet"
+DatasetSPSENonTenderSPMK = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSENonTenderEkontrakSPMKSPP{tahun}.parquet"
+DatasetSPSENonTenderBAST = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSENonTenderEkontrakBAPBAST{tahun}.parquet"
+
+### Dataset Pencatatan
+DatasetCatatNonTender = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSEPencatatanNonTender{tahun}.parquet"
+DatasetCatatNonTenderRealisasi = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSEPencatatanNonTenderRealisasi{tahun}.parquet"
+DatasetCatatSwakelola = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSEPencatatanSwakelola{tahun}.parquet"
+DatasetCatatSwakelolaRealisasi = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSEPencatatanSwakelolaRealisasi{tahun}.parquet"
+
+### Dataset Peserta Tender
+DatasetPesertaTender = "https://storage.googleapis.com/bukanamel/{kodeFolder}/spse/SPSEPesertaTender{tahun}.parquet"
+
+## Buat dataframe SPSE
+### Baca file parquet dataset SPSE Tender
+try:
+    df_SPSETenderPengumuman = pd.read_parquet(DatasetSPSETenderPengumuman)
+    df_SPSETenderSelesai = pd.read_parquet(DatasetSPSETenderSelesai)
+    df_SPSETenderSelesaiNilai = pd.read_parquet(DatasetSPSETenderSelesaiNilai)
+    df_SPSETenderSPPBJ = pd.read_parquet(DatasetSPSETenderSPPBJ)
+    df_SPSETenderKontrak = pd.read_parquet(DatasetSPSETenderKontrak)
+    df_SPSETenderSPMK = pd.read_parquet(DatasetSPSETenderSPMK)
+    df_SPSETenderBAST = pd.read_parquet(DatasetSPSETenderBAST)
+
+except Exception:
+    st.error("Gagal baca dataset SPSE Tender")
+
+### Baca file parquet dataset SPSE Non Tender
+try:
+    df_SPSENonTenderPengumuman = pd.read_parquet(DatasetSPSENonTenderPengumuman)
+    df_SPSENonTenderSelesai = pd.read_parquet(DatasetSPSENonTenderSelesai)
+    df_SPSENonTenderSPPBJ = pd.read_parquet(DatasetSPSENonTenderSPPBJ)
+    df_SPSENonTenderKontrak = pd.read_parquet(DatasetSPSENonTenderKontrak)
+    df_SPSENonTenderSPMK = pd.read_parquet(DatasetSPSENonTenderSPMK)
+    df_SPSENonTenderBAST = pd.read_parquet(DatasetSPSENonTenderBAST)
+
+except Exception:
+    st.error("Gagal baca dataset SPSE Non Tender")
+
+### Baca file parquet dataset Pencatatan
+try:
+    df_CatatNonTender = pd.read_parquet(DatasetCatatNonTender)
+    df_CatatNonTenderRealisasi = pd.read_parquet(DatasetCatatNonTenderRealisasi)
+    df_CatatSwakelola = pd.read_parquet(DatasetCatatSwakelola)
+    df_CatatSwakelolaRealisasi = pd.read_parquet(DatasetCatatSwakelolaRealisasi)
+
+except Exception:
+    st.error("Gagal baca dataset Pencatatan")
+
+### Baca file parquet dataset Peserta Tender
+try:
+    df_PesertaTender = pd.read_parquet(DatasetPesertaTender)
+
+except Exception:
+    st.error("Gagal baca dataset Peserta Tender")
+
+#####
+# Mulai membuat presentasi data SPSE
+#####
+
+# Buat menu yang mau disajikan
+menu_spse_1, menu_spse_2, menu_spse_3, menu_spse_4 = st.tabs(["| Tender |", "| Non Tender |", "| Pencatatan Non Tender |", "| Peserta Tender |"])
+
+## Tab menu SPSE - Tender
+with menu_spse_1:
+
+    st.header("SPSE - Tender")
+
+## Tab menu SPSE - Non Tender
+with menu_spse_2:
+
+    st.header("SPSE - Non Tender")
+
+## Tab menu SPSE - Pencatatan
+with menu_spse_3:
+
+    st.header("SPSE - Pencatatan")
+
+## Tab menu SPSE - Peserta Tender
+with menu_spse_4:
+
+    st.header("SPSE - Peserta Tender")
