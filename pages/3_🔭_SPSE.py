46fd14a7ee6c5dbc7a76d0978599cf24f9112ba7
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2023-10-23 11:55:50 +0700

    update code spse

--- pages/3_🔭_SPSE.py
+++ pages/3_🔭_SPSE.py
@@ -564,7 +564,8 @@ with menu_spse_1:
         with SPSE_SPPBJ_radio_1:
             status_kontrak = st.radio("**Status Kontrak**", ["Kontrak Selesai", "Kontrak Sedang Berjalan"])
         with SPSE_SPPBJ_radio_2:
-            opd = st.selectbox("Pilih Perangkat Daerah :", df_SPSETenderSPPBJ.unique(), key='opd_sppbj')
+
+            opd = st.selectbox("Pilih Perangkat Daerah :", df_SPSETenderSPPBJ['nama_satker'].unique(), key='opd_sppbj')
 
         
 
