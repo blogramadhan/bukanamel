56624087e13db6374e07c3de287ac17e8281dcbd
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2023-11-09 12:44:54 +0700

    update code spse

--- pages/3_🔭_SPSE.py
+++ pages/3_🔭_SPSE.py
@@ -930,7 +930,7 @@ with menu_spse_4:
 
     st.divider()
 
-    sumber_dana_pt = st.radio("**Sumber Dana :**", ["APBD", "APBDP", "BLUD"], key="PesertaTender")
+    sumber_dana_pt = st.radio("**Sumber Dana :**", df_PesertaTenderDetail_2['sumber_dana'].unique(), key="PesertaTender")
     st.write(f"Anda memilih : **{sumber_dana_pt}**")
 
     ### Tes
@@ -939,7 +939,7 @@ with menu_spse_4:
     ###
 
     #### Hitung-hitungan dataset Peserta Tender
-    df_PesertaTenderDetail_filter = con.execute(f"SELECT * FROM df_PesertaTenderDetail_2 WHERE sumber_dana = '{sumber_dana_pt}' AND nama_penyedia IS NOT NULL").df()
+    df_PesertaTenderDetail_filter = con.execute(f"SELECT * FROM df_PesertaTenderDetail_2 WHERE sumber_dana = '{sumber_dana_pt}'").df()
     st.dataframe(df_PesertaTenderDetail_filter)
     #jumlah_PesertaTender_daftar = con.execute(f"SELECT * FROM df_PesertaTenderDetail_filter WHERE nilai_penawaran = 0 AND nilai_terkoreksi = 0").df()
     #jumlah_PesertaTender_nawar = con.execute(f"SELECT * FROM df_PesertaTenderDetail_filter WHERE nilai_penawaran != 0 AND nilai_terkoreksi != 0").df()
