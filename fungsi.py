dc4014489df521973299470a99dd1a73f829b95a
Kurnia Ramadhan <kramadhan@gmail.com>
2023-11-09 22:47:25 +0700

    update code

--- fungsi.py
+++ fungsi.py
@@ -6,10 +6,10 @@ import pandas as pd
 def unduh_data(unduhdata):
     return unduhdata.to_csv(index=False).encode('utf')
 
-@st.cache_data(ttl=(12*3600))
+@st.cache_data(ttl=(4*3600))
 def tarik_data_excel(url):
     return pd.read_excel(url)
 
-@st.cache_data(ttl=(12*3600))
+@st.cache_data(ttl=(4*3600))
 def tarik_data(url):
     return pd.read_parquet(url)
