4e498d159db0e4f3bebf25fe0f1d7457b35c3166
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2023-11-09 12:42:21 +0700

    update code spse

--- pages/3_🔭_SPSE.py
+++ pages/3_🔭_SPSE.py
@@ -939,7 +939,8 @@ with menu_spse_4:
     ###
 
     #### Hitung-hitungan dataset Peserta Tender
-    #df_PesertaTenderDetail_filter = con.execute(f"SELECT * FROM df_PesertaTenderDetail_2 WHERE sumber_dana = '{sumber_dana_pt}' AND nama_penyedia IS NOT NULL").df()
+    df_PesertaTenderDetail_filter = con.execute(f"SELECT * FROM df_PesertaTenderDetail_2 WHERE sumber_dana = '{sumber_dana_pt}' AND nama_penyedia IS NOT NULL").df()
+    st.dataframe(df_PesertaTenderDetail_filter)
     #jumlah_PesertaTender_daftar = con.execute(f"SELECT * FROM df_PesertaTenderDetail_filter WHERE nilai_penawaran = 0 AND nilai_terkoreksi = 0").df()
     #jumlah_PesertaTender_nawar = con.execute(f"SELECT * FROM df_PesertaTenderDetail_filter WHERE nilai_penawaran != 0 AND nilai_terkoreksi != 0").df()
     #jumlah_PesertaTender_menang = con.execute(f"SELECT * FROM df_PesertaTenderDetail_filter WHERE pemenang = 1").df()
