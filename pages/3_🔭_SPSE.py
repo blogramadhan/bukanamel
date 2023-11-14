caf5e45a1c4cb5921a35534f3a920de68d08af3a
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2023-11-14 09:29:05 +0700

    update code spse

--- pages/3_🔭_SPSE.py
+++ pages/3_🔭_SPSE.py
@@ -864,7 +864,7 @@ with menu_spse_3:
 
         sql_CatatNonTender_query = f"""
             SELECT nama_paket AS NAMA_PAKET, jenis_realisasi AS JENIS_REALISASI, no_realisasi AS NO_REALISASI, tgl_realisasi AS TGL_REALISASI, pagu AS PAGU,
-            total_realisasi AS TOTAL_REALISASI, nilai_realisasi AS NILAI_REALISASI FROM df_CatatNonTender_OK_filter
+            total_realisasi AS TOTAL_REALISASI, nilai_realisasi AS NILAI_REALISASI, kategori_pengadaan, mtd_pemilihan FROM df_CatatNonTender_OK_filter
             WHERE status_nontender_pct_ket = '{status_nontender_cnt}' AND
             nama_satker = '{status_opd_cnt}'
         """
