9a6fe3fd5c302d384d58057d14cde9e435547718
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2023-12-12 14:36:56 +0700

    update code purchasing

--- pages/4_🗳️_E-PURCHASING.py
+++ pages/4_🗳️_E-PURCHASING.py
@@ -428,9 +428,9 @@ with menu_purchasing_1:
 
             st.divider()
 
-            ETALASE_radio_1, ETALASE_radio_2, ETALASE_radio_3, ETALASE_radio_4 = st.columns((1,1,2,6))
-            with ETALASE_radio_1:
-                jenis_katalog_etalase = st.radio("**Jenis Katalog**", ["Lokal", "Nasional", "Sektoral", "Gabungan"])
+            #ETALASE_radio_1, ETALASE_radio_2, ETALASE_radio_3, ETALASE_radio_4 = st.columns((1,1,2,6))
+            #with ETALASE_radio_1:
+            #    jenis_katalog_etalase = st.radio("**Jenis Katalog**", ["Lokal", "Nasional", "Sektoral", "Gabungan"])
             #with ETALASE_radio_2:
             #    nama_sumber_dana_etalase = st.radio("**Sumber Dana**", ["APBD", "APBDP", "APBN", "APBNP", "BLUD", "BLU", "BUMN", "BUMD"])
             #with ETALASE_radio_3:
