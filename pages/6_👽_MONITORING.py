80ea5dfb29f136090551e77f22b556efc967334a
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2023-12-19 14:08:17 +0700

    update

--- pages/6_👽_MONITORING.py
+++ pages/6_👽_MONITORING.py
@@ -200,7 +200,7 @@ with menu_monitoring_2:
 
             st.divider()
 
-            df_SIKAPNonTender_OK_filter = con.execute("SELECT kd_nontender, nama_paket, jenis_pengadaan, AVG(total_skors) FROM df_SIKAPNonTender_OK GROUP BY kd_nontender, nama_paket, jenis_pengadaan").df()
+            df_SIKAPNonTender_OK_filter = con.execute("SELECT kd_nontender AS KODE_PAKET, nama_paket, NAMA_PAKET, jenis_pengadaan AS JENIS_PENGADAAN, AVG(total_skors) AS SKOR_PENILAIAN FROM df_SIKAPNonTender_OK GROUP BY KODE_PAKET, NAMA_PAKET, JENIS_PENGADAAN").df()
             AgGrid(df_SIKAPNonTender_OK_filter)
 
         except Exception:
