da248978618453d15277263d42d0c36828a752c5
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2024-01-04 09:19:00 +0700

    update code purchasing

--- pages/4_🗳️_E-PURCHASING.py
+++ pages/4_🗳️_E-PURCHASING.py
@@ -566,7 +566,7 @@ with menu_purchasing_1:
 
         with menu_purchasing_1_3:
 
-            df_ECAT_OK_PIVOT = con.execute("SELECT nama_etalase, jenis_katalog, total_harga FROM df_ECAT_OK").df()
+            df_ECAT_OK_PIVOT = con.execute("SELECT nama_komoditas, jenis_katalog, total_harga FROM df_ECAT_OK").df()
             #df_ECAT_Pivot_Tabel = con.execute("PIVOT df_ECAT_OK_Pivot ON jenis_katalog USING SUM(total_harga)").df().fillna(0)
 
             # etalasepivot1, etalasepivot2 = st.columns((8,2))
