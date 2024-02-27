0f7b7c82ab7228ea30e7b35032fb92efa7b76d08
Kurnia Ramadhan <kramadhan@gmail.com>
2024-02-27 19:54:00 +0700

    update code

--- pages/2_📋_SIRUP.py
+++ pages/2_📋_SIRUP.py
@@ -142,12 +142,6 @@ try:
     df_RUPPP_umumkan_ukm = con.execute("SELECT * FROM df_RUPPP_umumkan WHERE status_ukm = 'UKM'").df()
     df_RUPPP_umumkan_pdn = con.execute("SELECT * FROM df_RUPPP_umumkan WHERE status_pdn = 'PDN'").df()
 
-    ### Query RUP Paket Penyedia (Pandas)
-    # df_RUPPP_umumkan = df_RUPPP[(df_RUPPP['status_umumkan_rup'] == 'Terumumkan') & (df_RUPPP['status_aktif_rup'] == 'TRUE')]
-    # df_RUPPP_belum_umumkan = df_RUPPP[df_RUPPP['status_umumkan_rup'] == 'Terinisiasi']
-    # df_RUPPP_umumkan_ukm = df_RUPPP[df_RUPPP['status_ukm'] == 'UKM']
-    # df_RUPPP_umumkan_pdn = df_RUPPP[df_RUPPP['status_pdn'] == 'PDN']
-
     namaopd = df_RUPPP_umumkan['nama_satker'].unique()
 
 except Exception:
@@ -158,10 +152,7 @@ try:
     df_RUPPS = tarik_data_pd(DatasetRUPPS)
 
     ### Query RUP Paket Swakelola
-    # df_RUPPS_umumkan = con.execute("SELECT * FROM df_RUPPS WHERE status_umumkan_rup = 'Terumumkan'").df()
-
-    ### Query RUP Paket Swakelola (Pandas)
-    df_RUPPS_umumkan = df_RUPPS[df_RUPPS['status_umumkan_rup'] == 'Terumumkan']
+    df_RUPPS_umumkan = con.execute("SELECT * FROM df_RUPPS WHERE status_umumkan_rup = 'Terumumkan'").df()
 
 except Exception:
     st.error("Gagal baca dataset RUP Paket Swakelola.")
